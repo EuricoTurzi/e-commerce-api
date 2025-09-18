cd7f0f88912313056b324de4fafff2f542f33605
Eurico <riicodt@gmail.com>
2025-09-17 22:48:32 -0300

    feat: update package.json to include tsc-watch

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express(); 
 
 app.get('/', (req, res)=>{
-    res.send('Teste de servidor node com express')
+    res.send('Teste de servidor node com express - Com o TSCWatch')
 })
 
 app.listen(3333, ()=>{
