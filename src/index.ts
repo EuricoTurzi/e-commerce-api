41fb009dd79f178ad229c2ab437a91f8647e3100
Eurico <riicodt@gmail.com>
2025-09-17 23:47:23 -0300

    feat: implement user management endpoints in Express API (using memory data)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,68 @@
-import express from 'express'; 
+import express, {Request, Response} from 'express'; 
 
+// Criar uma instância do express
 const app = express(); 
 
-app.get('/', (req, res)=>{
-    res.send('Teste de servidor node com express - Com o TSCWatch')
+// Permitir que o express entenda JSON no body das requisições
+app.use(express.json()) 
+
+// Definir o tipo de dado User
+type User = {
+    id: number,
+    name: string,
+    email: string
+}
+
+// Array para armazenar os usuários (em memória)
+let id = 0
+let users: User[] = []
+
+// Endpoint para retornar uma lista de usuários (Teste de endpoint com Postman)
+app.get('/users', (req: Request, res: Response)=>{
+    res.send(users)
+});
+
+// Endpoint para retornar um usuário específico pelo ID (Teste de endpoint com Postman)
+app.get('/users/:id', (req: Request, res: Response)=>{
+    let userId = Number(req.params.id)
+    let user = users.find(user => user.id === userId)
+    res.send(user)
+});
+
+
+// Endpoint para atualizar um usuário específico pelo ID (Teste de endpoint com Postman)
+app.put('/users/:id', (req: Request, res: Response)=>{
+    let userId = Number(req.params.id)
+    let user = req.body
+    let index = users.findIndex(user => user.id === userId)
+    users[index].name = user.name
+    users[index].email = user.email
+    res.send({
+        message: "Usuário atualizado com sucesso",
+    })
 })
 
-app.listen(3333, ()=>{
-    console.log('Server ativo na porta 3333')
+// Endpoint para deletar um usuário específico pelo ID (Teste de endpoint com Postman)
+app.delete('/users/:id', (req: Request, res: Response)=>{
+    let userId = Number(req.params.id)
+    users = users.filter(user => user.id !== userId)
+    res.send({
+        message: "Usuário deletado com sucesso",
+    })
+});
+
+// Endpoint para adicionar um novo usuário (Teste de endpoint com Postman)
+app.post('/users', (req: Request, res: Response)=>{
+    let user = req.body
+    user.id = ++id;
+    users.push(user)
+    res.send({
+        message: "Usuário adicionado com sucesso",
+    })
+})
+
+// Iniciar o servidor
+app.listen(3000, ()=>{
+    console.log('Servidor rodando na porta 3000');
 })
 
